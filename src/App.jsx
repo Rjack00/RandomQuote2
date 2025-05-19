6c3b8b1481b9e5fa91d28a71526d2997582c2e3f
Rjack00 <Rjackson77@comcast.net>
2025-05-19 16:06:52 -0600

    App.jsx updated
    App updated for RandomQuote functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { renderToPipeableStream } from 'react-dom/server';
+
+const api_url =   "https://quoteslate.vercel.app/api/quotes/random";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  useEffect(() => {
+    fetchData(api_url);
+  }, []);
+
+  const fetchData = async (url) => {
+    try{
+       const response = await fetch(url);
+      if(!response.ok){
+        throw new Error(`Response status: ${response.status}`);
+      }
+   
+    let data = await response.json();
+    const quoteText = data.quote || data.quotes?.[0] || data;
+      const quoteAuth = data.author || data.author?.[0] || data;
+    console.log("data: ",quoteText);
+    console.log("quoteAuthor: ",quoteAuth);
+    setQuote(quoteText);
+    setAuthor(quoteAuth);
+   } catch (error) {
+    console.error(`My Error message: ${error.message}`)
+   }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div id="quote-box">
+        <h1 id="title">Random Quotes</h1>
+      <h2 id="text">{quote}</h2>
+      <p id="author">{author}</p>
+      <button id="new-quote" onClick={() => fetchData(api_url)} >New Quote</button>
+      <a id="tweet-quote" href="twitter.com/intent/tweet">Tweet Quote</a>
+    </div>
+    );
+};
 
 export default App
